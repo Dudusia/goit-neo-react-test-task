543b65bb9bf4a570266f6259f8762ebbf3208217
Yevdokiia Mazhak <yevdokiia.mazhak@oag.com>
2025-09-03 12:30:04 +0300

    ci: add tf for deploying to vercel

---
+++ tf/main.tf
@@ -0,0 +1,24 @@
+# Namings
+module "this" {
+  source      = "cloudposse/label/null"
+  version     = "0.25.0"
+  namespace   = "goit-react"
+  environment = "test-task"
+  stage       = "prd"
+}
+
+resource "vercel_project" "this" {
+  name      = module.this.id_full
+  framework = "vite"
+
+  git_repository = {
+    type = "github"
+    repo = var.github_repo_name
+  }
+}
+
+resource "vercel_deployment" "this" {
+  project_id = vercel_project.this.id
+  ref        = "main"
+  production = tobool(module.this.stage == "prd")
+}   
---
+++ tf/providers.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_providers {
+    vercel = {
+      source = "vercel/vercel"
+      version = "~> 3.13"
+    }
+  }
+}
+
+provider "vercel" {}
