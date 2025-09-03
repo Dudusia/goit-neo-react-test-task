ca219644a8c97c37de318124b92887fa1d1e8c53
Yevdokiia Mazhak <yevdokiia.mazhak@oag.com>
2025-09-03 11:26:15 +0300

    feat: add navigation and basic styling vars

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,5 +1,32 @@
-import css from "./Navigation.module.css";
+import { NavLink } from 'react-router-dom';
+import icons from '../../assets/icons.svg';
+import css from './Navigation.module.css';
 
 export default function Navigation() {
-    return (<div>Navigation</div>)
+  return (
+    <header>
+      <div className={[css['container'], css['header-container']].join(' ')}>
+        <NavLink to="/">
+          <svg width="136" height="16" className={css['header-logo']}>
+            <use href={`${icons}#icon-logo`}></use>
+          </svg>
+        </NavLink>
+        <nav>
+          <ul className={css['navigation-list']}>
+            <li
+              className={[
+                css['navigation-list-item-accent'],
+                css['navigation-list-item'],
+              ].join(' ')}
+            >
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li className={css['navigation-list-item']}>
+              <NavLink to="/catalog">Catalog</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 }
