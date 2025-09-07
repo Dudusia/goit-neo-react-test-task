1365366e1ef8016f9dedfa4025fcb30e79ad51a6
Yevdokiia Mazhak <yevdokiia.mazhak@oag.com>
2025-09-08 00:51:10 +0300

    fix: svgs getting filled in fully black

--- src/components/CamperFeature/CamperFeature.jsx
+++ src/components/CamperFeature/CamperFeature.jsx
@@ -5,7 +5,7 @@ export default function CamperFeature({ iconId, displayValue }) {
   return (
     // TODO: maybe fine to just use li as a wrapper
     <div className={css['camper-feature-wrapper']}>
-      <svg width="20" height="20" className={css['camper-feature-icon']}>
+      <svg width="20" height="20">
         <use href={`${icons}#${iconId}`}></use>
       </svg>
       <p className={css['camper-feature-description']}>{displayValue}</p>
